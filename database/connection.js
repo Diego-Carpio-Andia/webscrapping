066784cde982db9c36f83f66d29174e227cb3a99
Diego Carpio <u201714271@upc.edu.pe>
2024-02-10 20:43:19 -0500

    Completo

--- database/connection.js
+++ database/connection.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
+
+const conexion = "mongodb://webscrappingnodejs:5OIV4PrawP7Al0PkVeo2fd6WNyPBm5AAjyk5Oaw4TylWZHw6lb25VzHHJTYlCWhbpEGTihdTLW0wACDbd4S7BA==@webscrappingnodejs.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@webscrappingnodejs@"
+
 const connection = async() => {
     try {
         mongoose.set('strictQuery', true);
-        await mongoose.connect("mongodb://localhost:27017/WebScraping");
+        await mongoose.connect(conexion);
         console.log("nos conectamos a la base de datos WebScraping");        
     } catch (error) {
         console.log(error);
